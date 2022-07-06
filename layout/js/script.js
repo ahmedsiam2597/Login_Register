873631fcfaa40a5a10912e2ede2244f45c9345ae
Ahmed Siam <ahmedsiam2597@gmail.com>
2022-07-06 08:31:32 +0200

    upload the design

--- layout/js/script.js
+++ layout/js/script.js
@@ -2,9 +2,11 @@ let loginBtn = document.querySelector("#loginBtn");
 let regBtn = document.querySelector("#registerBtn");
 let formBx = document.querySelector("#formBx");
 
-regBtn.addEventListener("click" , ()=>{
-    formBx.classList.add("active");
+regBtn.addEventListener("click", () => {
+  formBx.classList.add("active");
+  document.body.classList.add("colorBg");
 });
 loginBtn.addEventListener("click", () => {
   formBx.classList.remove("active");
+  document.body.classList.remove("colorBg");
 });
